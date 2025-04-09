b910e83a5b465120ecad26c0a8e9613150faff86
rahulforweb <rahul.maddela127@gmail.com>
2025-04-09 23:16:42 +0530

    Add files via upload

---
+++ files/src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h1>About Me</h1>
+      <p>
+        Hello! I'm Rahul, a passionate web developer who loves building
+        interactive websites and applications.
+      </p>
+    </section>
+  );
+};
+
+export default About;
---
+++ files/src/components/Skills.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section id="skills">
+      <h1>Skills</h1>
+      <p>Here are the technologies I work with:</p>
+      <ul>
+        <li>JavaScript</li>
+        <li>React.js</li>
+        <li>Node.js</li>
+        <li>CSS & TailwindCSS</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
